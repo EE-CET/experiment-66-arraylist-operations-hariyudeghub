63f00a2f37d36a5318cba7e8731142f16476c47a
hariyudeghub <harikrishnas9a@gmail.com>
2026-03-27 10:26:44 +0530

    main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class ArrayListOperations {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
